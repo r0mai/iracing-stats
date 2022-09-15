9c1bda29be2c5f4eb30b00b3339f7f90c0698d61
Andras Kucsma <andras.kucsma@prezi.com>
2022-09-15 18:21:54 +0200

    It works

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import sys
 import requests
 import hashlib
 import base64
+import json
 
 BASEURL = 'https://members-ng.iracing.com'
 
@@ -12,14 +13,17 @@ def get(s, suffix, params):
         print('Request {0} {1} failed'.format(suffix, params), res)
         print(res.json())
         raise 'error'
-    return res.json()
+    return res
+
+def get_json(s, suffix, params):
+    return get(s, suffix, params).json()
 
 def get_and_read(s, suffix, params):
-    res = get(s, suffix, params)
+    res = get_json(s, suffix, params)
     return s.get(res['link']).json()
 
 def get_and_read_chunked(s, suffix, params):
-    res = get(s, suffix, params)
+    res = get_json(s, suffix, params)
     chunk_info = res['data']['chunk_info']
     base_url = chunk_info['base_download_url']
     result_array = []
@@ -44,7 +48,19 @@ def search_series(s, cust_id, year, quarter):
     })
 
 def get_session_results(s, subsession_id):
-    print(get_and_read(s, '/results/get/', {'subsession_id': subsession_id}))
+    return get_and_read(s, '/data/results/get/', {'subsession_id': subsession_id})
+
+def get_time_spent_in_session(session_results, cust_id):
+    time_spent = 0
+    for session_result in session_results['session_results']:
+        for player in session_result['results']:
+            if player['cust_id'] == cust_id:
+                time_spent += player['average_lap'] * player['laps_complete']
+
+    return time_spent
+
+def get_series_name(session_results):
+    return session_results['season_name']
 
 def encode_pw(username, password):
     initialHash = hashlib.sha256((password + username.lower()).encode('utf-8')).digest()
@@ -57,7 +73,7 @@ def auth(s):
     user = os.getenv('IRACING_USER')
     token = os.getenv('IRACING_TOKEN')
 
-    res = s.post(BASEURL + '/auth', data={'email':user, 'password': token})
+    res = s.post(BASEURL + '/auth', data={'email': user, 'password': token})
     if res.status_code != 200:
         raise 'auth error'
 
@@ -67,4 +83,13 @@ if __name__ == '__main__':
     auth(s)
     cust_id = get_cust_id(s, sys.argv[1])
     series = search_series(s, cust_id, 2022, 3)
-    get_session_results(s, series[0]['subsession_id'])
+
+    time_spent = 0
+    for ser in series:
+        session_result = get_session_results(s, ser['subsession_id'])
+
+        print('Processing {0} ({1})'.format(get_series_name(session_result), ser['subsession_id']))
+
+        time_spent += get_time_spent_in_session(session_result, cust_id)
+
+    print('Time spent: {0}s'.format(time_spent / 10000))
