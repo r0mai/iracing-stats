12d9ab8b731fcc296560e5447ad540807cadeb56
Andras Kucsma <andras.kucsma@prezi.com>
2022-09-16 13:55:48 +0200

    Length driven stats

--- main.py
+++ main.py
@@ -69,11 +69,36 @@ def get_time_spent_in_session(session_results, cust_id):
 
     return time_spent
 
+def get_laps_completed_in_session(session_results, cust_id):
+    laps_completed = 0
+    for session_result in session_results['session_results']:
+        for player in session_result['results']:
+            if player['cust_id'] == cust_id:
+                laps_completed += player['laps_complete']
+
+    return laps_completed 
+
 def get_series_name(session_results):
     return session_results['season_name']
 
 def get_start_time(session_results):
     return session_results['start_time']
+
+def get_track_id(session_results):
+    return session_results['track']['track_id']
+
+def get_track_infos(s):
+    return get_and_read(s, '/data/track/get', {})
+
+def get_track_length(track_infos, track_id):
+    for track in track_infos:
+        if track['track_id'] == track_id:
+            return track['track_config_length']
+
+def get_track_name(track_infos, track_id):
+    for track in track_infos:
+        if track['track_id'] == track_id:
+            return '{0} -- {1}'.format(track['track_name'], track['config_name'])
 
 def encode_pw(username, password):
     initialHash = hashlib.sha256((password + username.lower()).encode('utf-8')).digest()
@@ -95,23 +120,33 @@ if __name__ == '__main__':
     s = requests.Session()
     auth(s)
     cust_id = get_cust_id(s, sys.argv[1])
+    track_infos = get_track_infos(s)
 
     time_spent = 0
+    length_driven = 0
     for quarter in range(1, 4+1):
         series = search_series(s, cust_id, 2022, quarter)
 
         for ser in series:
             session_result = get_session_results(s, ser['subsession_id'])
 
+            track_id = get_track_id(session_result)
+            track_length = get_track_length(track_infos, track_id)
             time = get_time_spent_in_session(session_result, cust_id)
+            kms = track_length * get_laps_completed_in_session(session_result, cust_id)
 
-            print('Processing {0} {1} ({2}) -- {3}s'.format(
+            print('Processing {0} {1} ({2}) -- {3}s | {4}km'.format(
                 get_start_time(session_result),
                 get_series_name(session_result),
                 ser['subsession_id'],
-                time / 10000)
+                time / 10000,
+                kms)
             )
 
             time_spent += time
+            length_driven += kms
 
-    print('Time spent: {0}s'.format(time_spent / 10000))
+    hours = time_spent / 10000 / 60 / 60
+    print('Time spent: {0:.1f} hours'.format(hours))
+    print('Length driven: {0:.1f}km'.format(length_driven))
+    print('Average speed: {0:.1f}km/h'.format(length_driven / hours))
