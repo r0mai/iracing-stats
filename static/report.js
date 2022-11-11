32a7aec9fc4db73c92623ec272ba843534319f5d
András Kucsma <r0maikx02b@gmail.com>
2022-11-12 00:44:06 +0100

    By-date and by-race graphs

--- static/report.js
+++ static/report.js
@@ -1,8 +1,4 @@
-async function updateIratingHistory(div, driverName) {
-    console.log('update irating', div, driverName);
-    let resp = await fetch('/api/v1/irating-history?driver_name=' + driverName);
-    let result = await resp.json()
-
+function populateIratingHistoryDate(dateDiv, data) {
     graphData = {
         x: [],
         y: [],
@@ -13,14 +9,44 @@ async function updateIratingHistory(div, driverName) {
             '%{text}'
     };
 
-    result.forEach(race => {
+    data.forEach(race => {
         graphData.x.push(new Date(race['start_time'] * 1000));
         graphData.y.push(race['irating']);
         graphData.text.push(race['series_name']);
     });
 
-    Plotly.newPlot(iratingHistoryDiv, [graphData], {
+    Plotly.newPlot(dateDiv, [graphData], {
         margin: { t: 0 }
     });
 }
 
+function populateIratingHistoryRace(raceDiv, data) {
+    graphData = {
+        x: [],
+        y: [],
+        text: [],
+        hovertemplate:
+            'Date: %{x}<br>' +
+            'iRating: %{y}<br>' +
+            '%{text}'
+    };
+
+    var i = 0;
+    data.forEach(race => {
+        graphData.x.push(i++);
+        graphData.y.push(race['irating']);
+        graphData.text.push(race['series_name']);
+    });
+
+    Plotly.newPlot(raceDiv, [graphData], {
+        margin: { t: 0 }
+    });
+}
+
+async function updateIratingHistory(dateDiv, raceDiv, driverName) {
+    let resp = await fetch('/api/v1/irating-history?driver_name=' + driverName);
+    let result = await resp.json()
+
+    populateIratingHistoryDate(dateDiv, result);
+    populateIratingHistoryRace(raceDiv, result);
+}
