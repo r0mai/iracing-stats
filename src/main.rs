c7d2f9dbc9b67693f65346de1742d910d7b70c75
Andras Kucsma <r0maikx02b@gmail.com>
2022-12-16 23:59:15 +0100

    Rust: parse zipped jsons

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+use std::fs;
+use serde_json;
+
+fn read_single_file_zip(file_name: &str) -> String {
+    let zip_file = fs::File::open(file_name).unwrap();
+    let mut archive = zip::ZipArchive::new(zip_file).unwrap();
+
+    if archive.len() != 1 {
+        return "".to_owned();
+    }
+
+    let mut session_file = archive.by_index(0).unwrap();
+
+    return std::io::read_to_string(&mut session_file).unwrap();
+}
+
+fn parse_session_zip(zip_file: &str) {
+    let contents = read_single_file_zip(zip_file);
+    let data: serde_json::Value = serde_json::from_str(&contents).unwrap();
+
+    println!("{}", data["subsession_id"]);
+}
+
 fn main() {
-    println!("Hello, world!");
+    parse_session_zip("data/sessions/10004652.session.zip");
 }
