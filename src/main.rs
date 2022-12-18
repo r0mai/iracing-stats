3044c06407aac711e09520b837d48ebc1c56c625
Andras Kucsma <r0maikx02b@gmail.com>
2022-12-18 20:03:33 +0100

    Finish porting db rebuild to rust

--- src/main.rs
+++ src/main.rs
@@ -16,6 +16,10 @@ struct DbContext<'a> {
     insert_track_config_statement: rusqlite::Statement<'a>,
     insert_car_statement: rusqlite::Statement<'a>,
     insert_subsession_statement: rusqlite::Statement<'a>,
+    insert_session_statement: rusqlite::Statement<'a>,
+    insert_simsession_statement: rusqlite::Statement<'a>,
+    insert_driver_statement: rusqlite::Statement<'a>,
+    insert_driver_result_statement: rusqlite::Statement<'a>,
 }
 
 fn create_db_context<'a>(tx: &'a mut rusqlite::Transaction) -> DbContext<'a> {
@@ -45,13 +49,45 @@ fn create_db_context<'a>(tx: &'a mut rusqlite::Transaction) -> DbContext<'a> {
             ?, /* license_category_id */
             ?  /* track_id */
         );"#).unwrap();
+    let insert_session_statement = tx.prepare(r#"
+        INSERT OR IGNORE INTO session VALUES(
+            ?,  /* session_id */
+            ?   /* series_name */
+    );"#).unwrap();
+    let insert_simsession_statement = tx.prepare(r#"
+        INSERT INTO simsession VALUES(
+            ?, /* subsession_id */
+            ?, /* simsession_number */
+            ?  /* simsession_type */
+    );"#).unwrap();
+    let insert_driver_statement= tx.prepare(r#"
+        INSERT OR IGNORE INTO driver VALUES(
+            ?, /* cust_id */
+            ?  /* display_name */
+    );"#).unwrap();
+    let insert_driver_result_statement= tx.prepare(r#"
+        INSERT INTO driver_result VALUES(
+            ?, /* cust_id */
+            ?, /* team_id */
+            ?, /* subsession_id */
+            ?, /* simsession_number */
+            ?, /* newi_rating */
+            ?, /* incidents */
+            ?, /* laps_complete */
+            ?, /* average_lap */
+            ?  /* car_id */
+    );"#).unwrap();
 
     return DbContext {
         tx,
         insert_track_statement,
         insert_track_config_statement,
         insert_car_statement,
-        insert_subsession_statement
+        insert_subsession_statement,
+        insert_session_statement,
+        insert_simsession_statement,
+        insert_driver_statement,
+        insert_driver_result_statement,
     };
 }
 
@@ -87,12 +123,12 @@ fn build_db_schema(tx: &rusqlite::Transaction) {
 
 fn add_track_to_db(ctx: &mut DbContext, track: &serde_json::Value) {
     ctx.insert_track_statement.execute((
-        track["package_id"].as_u64().unwrap(),
+        track["package_id"].as_i64().unwrap(),
         track["track_name"].as_str().unwrap()
     )).unwrap();
     ctx.insert_track_config_statement.execute((
-        track["track_id"].as_u64().unwrap(),
-        track["package_id"].as_u64().unwrap(),
+        track["track_id"].as_i64().unwrap(),
+        track["package_id"].as_i64().unwrap(),
         track["config_name"].as_str().unwrap_or(""),
         track["track_config_length"].as_f64().unwrap()
     )).unwrap();
@@ -100,22 +136,78 @@ fn add_track_to_db(ctx: &mut DbContext, track: &serde_json::Value) {
 
 fn add_car_to_db(ctx: &mut DbContext, car: &serde_json::Value) {
     ctx.insert_car_statement.execute((
-        car["car_id"].as_u64().unwrap(),
+        car["car_id"].as_i64().unwrap(),
         car["car_name"].as_str().unwrap(),
         car["car_name_abbreviated"].as_str().unwrap(),
     )).unwrap();
 }
 
+fn add_driver_to_db(ctx: &mut DbContext, driver_result: &serde_json::Value) {
+    ctx.insert_driver_statement.execute((
+        driver_result["cust_id"].as_i64().unwrap(),
+        driver_result["display_name"].as_str().unwrap(),
+    )).unwrap();
+}
+
+fn add_driver_result_to_db(ctx: &mut DbContext, subsession_id: i64, simsession_number: i64, team_id: i64, driver_result: &serde_json::Value) {
+    add_driver_to_db(ctx, driver_result);
+
+    // TODO cust_id could be factored out
+
+    ctx.insert_driver_result_statement.execute((
+        driver_result["cust_id"].as_i64().unwrap(), 
+        team_id,
+        subsession_id,
+        simsession_number,
+        driver_result["newi_rating"].as_i64().unwrap(),
+        driver_result["incidents"].as_i64().unwrap(),
+        driver_result["laps_complete"].as_i64().unwrap(),
+        driver_result["average_lap"].as_i64().unwrap(),
+        driver_result["car_id"].as_i64().unwrap()
+    )).unwrap();
+}
+
+fn add_simsession_db(ctx: &mut DbContext, subsession_id: i64, simsession: &serde_json::Value) {
+    let simsession_number = simsession["simsession_number"].as_i64().unwrap();
+
+    ctx.insert_simsession_statement.execute((
+        subsession_id,
+        simsession_number,
+        simsession["simsession_type"].as_i64().unwrap()
+    )).unwrap();
+
+    for participant in simsession["results"].as_array().unwrap() {
+        if participant["cust_id"].as_i64().is_some() {
+            add_driver_result_to_db(ctx, subsession_id, simsession_number, 0, participant);
+        } else { // team
+            let team_id = participant["team_id"].as_i64().unwrap();
+            for driver in participant["driver_results"].as_array().unwrap() {
+                add_driver_result_to_db(ctx, subsession_id, simsession_number, team_id, driver);
+            }
+        }
+    }
+}
+
 fn add_subsession_to_db(ctx: &mut DbContext, subsession: &serde_json::Value) {
-    let subsession_id = subsession["subsession_id"].as_u64().unwrap();
+    let subsession_id = subsession["subsession_id"].as_i64().unwrap();
+    let session_id = subsession["session_id"].as_i64().unwrap();
 
     ctx.insert_subsession_statement.execute((
         subsession_id,
-        subsession["session_id"].as_u64().unwrap(),
+        session_id,
         parse_date(subsession["start_time"].as_str().unwrap()),
-        subsession["license_category_id"].as_u64().unwrap(),
-        subsession["track"]["track_id"].as_u64().unwrap()
+        subsession["license_category_id"].as_i64().unwrap(),
+        subsession["track"]["track_id"].as_i64().unwrap()
     )).unwrap();
+
+    ctx.insert_session_statement.execute((
+        session_id,
+        subsession["series_name"].as_str().unwrap()
+    )).unwrap();
+
+    for simsession in subsession["session_results"].as_array().unwrap() {
+        add_simsession_db(ctx, subsession_id, simsession);
+    }
 }
 
 fn add_sessions_to_db<I>(ctx: &mut DbContext, files: I) 
