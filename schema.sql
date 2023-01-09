8a3252d06544cf01d051109274c3e696cced7a4a
Andras Kucsma <r0maikx02b@gmail.com>
2023-01-09 02:08:13 +0100

    Add 0 based comment to schema

--- schema.sql
+++ schema.sql
@@ -30,8 +30,8 @@ CREATE TABLE driver_result(
     laps_complete INTEGER,
     average_lap INTEGER,
     car_id INTEGER,
-    finish_position INTEGER,
-    finish_position_in_class INTEGER,
+    finish_position INTEGER, /* 0 based! */
+    finish_position_in_class INTEGER, /* 0 based! */
     PRIMARY KEY(cust_id, team_id, subsession_id, simsession_number)
 );
 
