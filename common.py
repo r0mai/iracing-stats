fcb685fb0dae92e1e18f553624d9b62e9724b6a3
Andras Kucsma <r0maikx02b@gmail.com>
2023-01-18 00:31:24 +0100

    Remove legacy python stuff

--- common.py
+++
@@ -1,6 +0,0 @@
-SESSIONS_DIR = 'data/sessions'
-TRACK_DATA_FILE = 'data/tracks.json'
-CAR_DATA_FILE = 'data/cars.json'
-SQLITE_DB_FILE = 'stats.db'
-BASEURL = 'https://members-ng.iracing.com'
-SCHEMA_SQL = 'schema.sql'
