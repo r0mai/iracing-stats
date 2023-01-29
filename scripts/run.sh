70d39036b42c0fa64be38b4d1c44c1dc35bc69e4
Andras Kucsma <r0maikx02b@gmail.com>
2023-01-29 12:49:23 +0100

    Add run.sh to locally run a image

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run --mount type=volume,src=iracing-stats-dir,target=/iracing-stats-dir iracing-stats
+docker run -p 8000:8000 --mount type=volume,src=iracing-stats-dir,target=/iracing-stats-dir iracing-stats
