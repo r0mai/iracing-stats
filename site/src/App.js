dd7de204ff1a20b4df554e6fe0fb2844ed61016a
Andras Kucsma <r0maikx02b@gmail.com>
2023-02-06 22:33:48 +0100

    Read driver list from query params

--- site/src/App.js
+++ site/src/App.js
@@ -1,9 +1,18 @@
+import _default from 'react-async';
 import './App.css';
-import DriverList from './DriverList.js'
+import DriverList from './DriverList.js';
 
 function App() {
+    let paramString = window.location.search.split('?')[1];
+    let queryString = new URLSearchParams(paramString);
+
+    let urlDrivers = queryString.get('drivers');
+    if (!urlDrivers) {
+        return "Pass in a list of drivers <url>?drivers=Driver1;Driver2;Driver3";
+    }
+    let drivers = urlDrivers.split(';');
     return (
-        <DriverList drivers={["Andras Kucsma", "Gábor Rosta"]}/>
+        <DriverList drivers={drivers}/>
     );
 }
 
