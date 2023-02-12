eacff5418b17296d0de6b0ee5ea566316b427096
Andras Kucsma <r0maikx02b@gmail.com>
2023-02-13 00:45:12 +0100

    More Motec

--- src/motec_xml.rs
+++ src/motec_xml.rs
@@ -13,19 +13,22 @@ fn add_constant(element: &mut XMLElement, name: &str, value: &String, unit: &str
 }
 
 pub fn output_motec_track_xmls() {
-    let mut xml_file = File::create("tracks.xml").unwrap();
 
     let track_contents = read_to_string(get_track_data_file()).unwrap();
     let tracks: Value = serde_json::from_str(&track_contents).unwrap();
 
-    let mut root = XMLElement::new("Root");
-
     for track in tracks.as_array().unwrap() {
+        let dir_path = track["track_dirpath"].as_str().unwrap().replace('\\', " ");
+        let id = format!("track {}", dir_path);
+        let xml_name = format!("motec/{}.xml", id);
+
+        let xml_file = File::create(xml_name).unwrap();
+
         let mut maths = XMLElement::new("Maths");
-        // maths.add_attribute("Locale", "Hungarian_Hungary.1250");
-        // maths.add_attribute("DefaultLocale", "C");
-        maths.add_attribute("TrackName", track["track_name"].as_str().unwrap());
-        maths.add_attribute("ConfigName", track["config_name"].as_str().unwrap_or(""));
+        maths.add_attribute("Locale", "Hungarian_Hungary.1250");
+        maths.add_attribute("DefaultLocale", "C");
+        maths.add_attribute("Id", id);
+        maths.add_attribute("Condition", format!("'Venue' == \"{}\"", dir_path));
 
         let mut constants = XMLElement::new("MathConstants");
 
@@ -35,8 +38,7 @@ pub fn output_motec_track_xmls() {
         add_constant(&mut constants, "track_length", &track["track_config_length"].as_f64().unwrap().to_string(), "km");
 
         maths.add_child(constants);
-        root.add_child(maths);
+        maths.write(xml_file).unwrap();
     }
 
-    root.write(xml_file).unwrap();
 }
