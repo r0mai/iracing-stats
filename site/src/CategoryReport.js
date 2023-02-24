9ae468e057e5aff4d08f59039323fd199eafc5ce
Andras Kucsma <r0maikx02b@gmail.com>
2023-02-24 23:05:18 +0100

    Calculate highest achieved IRating

--- site/src/CategoryReport.js
+++ site/src/CategoryReport.js
@@ -1,9 +1,19 @@
 import IRatingHistory from './IRatingHistory.js'
 import IncidentHistory from './IncidentHistory.js'
+import { isCategory } from './Utility.js';
+import { Typography } from '@mui/material';
+
+function getHighestIRating(driverSessions, category) {
+    let filteredIRating = driverSessions.filter(session => isCategory(session, category)).map(session => session["new_irating"]);
+    return Math.max.apply(null, filteredIRating);
+}
 
 function CategoryReport({driverSessions, category}) {
     return (
         <div>
+            <Typography>
+                Highest IRating achieved: {getHighestIRating(driverSessions, category)}
+            </Typography>
             <IRatingHistory driverSessions={driverSessions} category={category}/>
             <IncidentHistory driverSessions={driverSessions} category={category}/>
         </div>
