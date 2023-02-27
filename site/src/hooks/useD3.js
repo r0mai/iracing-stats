d1fd096df799c33de8ffa9ed96da5e22055d056c
Andras Kucsma <r0maikx02b@gmail.com>
2023-02-27 21:23:30 +0100

    Clear out previous svgs in useD3

--- site/src/hooks/useD3.js
+++ site/src/hooks/useD3.js
@@ -4,6 +4,8 @@ export const useD3 = (renderChartFn, dependencies) => {
     const ref = React.useRef();
 
     React.useEffect(() => {
+        // Delete any previous svgs
+        ref.current.innerHTML = '';
         renderChartFn(ref.current);
         return () => {};
     }, dependencies);
