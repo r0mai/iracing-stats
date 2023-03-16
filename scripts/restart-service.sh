42d9a4f30c976d4741dc2c94853eb740c3882b56
Andras Kucsma <r0maikx02b@gmail.com>
2023-03-16 14:29:06 +0100

    Expose docker on port 80

--- scripts/restart-service.sh
+++ scripts/restart-service.sh
@@ -3,5 +3,5 @@
 ssh -i $AWS_PERM_FILE $AWS_NODE << EOF
     docker ps -aq | xargs --no-run-if-empty docker stop | xargs --no-run-if-empty docker rm
     docker load -i ~/iracing-stats.img
-    docker run --detach -p 8000:8000 --mount type=volume,src=iracing-stats-dir,target=/iracing-stats-dir iracing-stats
+    docker run --detach -p 80:8000 --mount type=volume,src=iracing-stats-dir,target=/iracing-stats-dir iracing-stats
 EOF
