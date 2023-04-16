f321dbbc2e0d7ba038e3f7dfde9b5c2982c90797
Andras Kucsma <r0maikx02b@gmail.com>
2023-04-16 13:59:45 +0200

    Create driver_result_team_id_index

--- src/indices.sql
+++ src/indices.sql
@@ -1,3 +1,5 @@
 CREATE INDEX driver_driver_name_index ON driver(display_name);
 
+CREATE INDEX driver_result_team_id_index ON driver_result(team_id);
+
 CREATE INDEX site_team_site_team_name_index ON site_team(site_team_name);
