3fc2edd19db1e986f0c3f9c3b208d2a8bdd48682
Andras Kucsma <r0maikx02b@gmail.com>
2023-12-06 21:46:48 +0100

    team-car-usage.sql

---
+++ queries/team-car-usage.sql
@@ -0,0 +1,36 @@
+.mode column
+.headers on
+SELECT
+    car_name as "car_name               ",
+    /* COUNT(DISTINCT subsession.subsession_id), */
+    /* round(SUM(track_config_length * driver_result.laps_complete)) */
+    round(SUM(driver_result.average_lap * driver_result.laps_complete))
+FROM
+    driver_result
+JOIN simsession ON
+    driver_result.subsession_id = simsession.subsession_id AND
+    driver_result.simsession_number = simsession.simsession_number
+JOIN subsession ON
+    simsession.subsession_id = subsession.subsession_id
+JOIN session ON
+    subsession.session_id = session.session_id
+JOIN track_config ON
+    subsession.track_id = track_config.track_id
+JOIN car ON
+    driver_result.car_id = car.car_id
+JOIN driver ON
+    driver.cust_id = driver_result.cust_id
+JOIN site_team_member ON
+    site_team_member.cust_id = driver.cust_id
+JOIN site_team ON
+    site_team.site_team_id = site_team_member.site_team_id
+WHERE
+    site_team_name = "rsmr" AND
+    simsession.simsession_type = 6 AND
+    /* driver_result.team_id != 0 AND */
+    subsession.start_time LIKE '2023%'
+GROUP BY
+    car.car_id
+ORDER BY
+    SUM(driver_result.average_lap * driver_result.laps_complete) DESC
+;
