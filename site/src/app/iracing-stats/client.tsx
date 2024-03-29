307738b2ea1b22c47b6e5ef9146222ec28a15f7b
Andras Kucsma <r0maikx02b@gmail.com>
2024-03-29 17:11:21 +0100

    Fix server side rendering issues

--- site/src/app/iracing-stats/client.tsx
+++ site/src/app/iracing-stats/client.tsx
@@ -1,12 +1,24 @@
 'use client'
  
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import dynamic from 'next/dynamic'
 import { BrowserRouter } from 'react-router-dom'
  
 const App = dynamic(() => import('../../App'), { ssr: false })
  
 export function ClientOnly() {
+  // Completely prevent server side rendering:
+  // https://nextjs.org/docs/messages/react-hydration-error
+  const [isClient, setIsClient] = useState(false)
+
+  useEffect(() => {
+    setIsClient(true)
+  }, [])
+
+  if (!isClient) {
+    return null;
+  }
+
   return (
     <React.StrictMode>
       <BrowserRouter>
