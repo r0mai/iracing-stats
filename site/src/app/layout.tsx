0d01486ca40c8e4808571ccbcf938039c6fbf1a3
Andras Kucsma <r0maikx02b@gmail.com>
2024-03-29 13:39:35 +0100

    Progress with nextjs migration

---
+++ site/src/app/layout.tsx
@@ -0,0 +1,29 @@
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <head>
+        <link rel="icon" href="%PUBLIC_URL%/favicon.ico" />
+        <meta name="theme-color" content="#000000" />
+        <meta
+          name="description"
+          content="Web site created using create-react-app"
+        />
+        <title>IRacing Stats</title>
+
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+        />
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+
+      </head>
+      <body>
+        <noscript>You need to enable JavaScript to run this app.</noscript>
+        <div id="root">{children}</div>
+      </body>
+    </html>
+}
