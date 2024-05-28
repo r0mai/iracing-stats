d6b4c90d48a6fcec936213ae81842975281c25f5
Andras Kucsma <r0maikx02b@gmail.com>
2024-05-28 19:07:30 +0200

    Add regex for new link pattern

--- site/public/iracing-stats/archive-helper.js
+++ site/public/iracing-stats/archive-helper.js
@@ -9,14 +9,17 @@ function replaceAll(str, search, replacement) {
 }
 
 async function refreshText() {
-    const regexp = /https:\/\/members.iracing.com\/membersite\/member\/EventResult\.do\?&?subsessionid=([0-9]+)/g;
-    let matches = [...textarea.value.matchAll(regexp)];
+    const regexp1 = /https:\/\/members.iracing.com\/membersite\/member\/EventResult\.do\?&?subsessionid=([0-9]+)/g;
+    const regexp2 = /https:\/\/members-ng.iracing.com\/racing\/home\/dashboard\?subsessionid=([0-9]+)/g;
+    let matches = [...textarea.value.matchAll(regexp1), ...textarea.value.matchAll(regexp2)];
     let subsession_ids = matches.map(e => e[1]);
     if (subsession_ids.length === 0) {
         return;
     }
 
     let unique_subsession_ids = [...new Set(subsession_ids)];
+
+    console.log(subsession_ids);
 
     let url = `/api/v1/session-result?team=rsmr&subsession_ids=` + unique_subsession_ids.join(";");
     let response = await fetch(url);
