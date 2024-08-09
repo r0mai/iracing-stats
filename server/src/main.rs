95930af2aa07a0f7445e991d15a31415c451a812
Andras Kucsma <r0maikx02b@gmail.com>
2024-08-09 13:22:07 +0200

    New test subsessionid list

--- server/src/main.rs
+++ server/src/main.rs
@@ -159,7 +159,8 @@ async fn tokio_main(args: &Args) {
         // discord_hook::send_discord_update(vec![63740038, 61486453, 61145537, 13059307, 64483246], true).await;
         // discord_hook::send_discord_update(vec![16936417i64], true).await; // This one has a weird reason_out
         // discord_hook::send_discord_update(vec![68005625], true).await;
-        discord_hook::send_discord_update(vec![58796522, 61486453, 70385102], true).await;
+        // discord_hook::send_discord_update(vec![58796522, 61486453, 70385102], true).await;
+        discord_hook::send_discord_update(vec![70546716], true).await;
     }
 
     if args.sync_site_teams_to_db_partial {
