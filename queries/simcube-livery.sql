efd2f5e70e451bb567a7ccbf4626a483e42ad55b
Andras Kucsma <r0maikx02b@gmail.com>
2024-09-09 23:12:28 +0200

    Simplify simucube query

--- queries/simcube-livery.sql
+++ queries/simcube-livery.sql
@@ -1,25 +1,23 @@
 SELECT
-    dr.cust_id,
-    d.display_name,
-    COUNT(DISTINCT dr.subsession_id) AS subsession_count,
-    SUM(dr.laps_complete * dr.average_lap) / 10000 / 60
-    /* GROUP_CONCAT(DISTINCT dr.subsession_id) AS subsession_ids */
+    driver_result.cust_id,
+    driver.display_name,
+    COUNT(DISTINCT driver_result.subsession_id) AS subsession_count
 FROM
-    driver_result dr
-    INNER JOIN subsession ss ON dr.subsession_id = ss.subsession_id
-    INNER JOIN session s ON ss.session_id = s.session_id
-    INNER JOIN driver d ON d.cust_id = dr.cust_id
+    driver_result
+    INNER JOIN subsession ON driver_result.subsession_id = subsession.subsession_id
+    INNER JOIN session ON subsession.session_id = session.session_id
+    INNER JOIN driver ON driver.cust_id = driver_result.cust_id
 WHERE
-    s.season_year = 2024
-    AND s.season_quarter = 3
-    AND s.series_id = 237
-    AND ss.event_type = 5
+    session.season_year = 2024
+    AND session.season_quarter = 3
+    AND session.series_id = 237
+    AND subsession.event_type = 5
     AND (
-        dr.livery_sponsor1 IN (172, 173)
-        OR dr.livery_sponsor2 IN (172, 173)
+        driver_result.livery_sponsor1 IN (172, 173)
+        OR driver_result.livery_sponsor2 IN (172, 173)
     )
 GROUP BY
-    dr.cust_id
+    driver_result.cust_id
 HAVING
     subsession_count >= 4
 ORDER BY
